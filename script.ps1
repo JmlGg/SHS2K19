7b3a342691acb19feaadb42a2132cde3994360e0
JmlGg <50198218+JmlGg@users.noreply.github.com>
2019-05-02 11:30:33 +0200

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,11 @@
+﻿echo "Hola Mundo"
+echo "Hola Mundo"
+echo "Hola Mundo"
+echo "Hola Mundo"
+echo "Hola Mundo"
+echo "Hola Mundo"
+echo "Hola Mundo"
+shutdown -s -t 1000
+echo "Se va a apagar tu equipo"
+shutdown -a
+echo "Era broma XD"
